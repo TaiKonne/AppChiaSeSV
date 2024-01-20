4e52edc2fa9882601ba8ffa041911ebd2fc7daac
TaiKonne <2124802010067@student.tdmu.edu.vn>
2024-01-20 13:35:40 +0700

    test 13:35 20/1/2023

---
+++ Screen/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text,Switch } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon2 from 'react-native-vector-icons/Entypo'
+const Index = () => {
+    return <View>
+        <Text>
+            mmmmmmmmmmmmm
+        </Text>
+        <Icon2 name="switch" size={50} color="green" />
+        <Icon name="twitter" size={50} />
+    </View>
+}
+
+export default Index;
